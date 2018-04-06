783e714f91672f05447a3a1175bf0ee7ce763ad8
Jordan Ott <jordanott365@gmail.com>
2018-04-05 21:35:33 -0700

    Create image

--- DataGeneration/text_to_image.py
+++ DataGeneration/text_to_image.py
@@ -1,1 +1,9 @@
 # text to image
+from PIL import Image, ImageDraw, ImageFont
+
+def create_img(text,name):
+    img = Image.new('RGB', (400, 400))
+    d = ImageDraw.Draw(img)
+    d.text((15, 15), text, fill=(255, 0, 0))
+
+    img.save(name + '.png')
