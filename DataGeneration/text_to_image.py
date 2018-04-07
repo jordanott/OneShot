20e962018efe00a687d0ba318e013f424c005839
Jordan Ott <jordanott365@gmail.com>
2018-04-06 17:24:36 -0700

    Resize images

--- DataGeneration/text_to_image.py
+++ DataGeneration/text_to_image.py
@@ -2,8 +2,7 @@
 from PIL import Image, ImageDraw, ImageFont
 
 def create_img(text,name):
-    img = Image.new('RGB', (400, 400))
+    img = Image.new('L', (400, 400))
     d = ImageDraw.Draw(img)
-    d.text((15, 15), text, fill=(255, 0, 0))
-
+    d.text((15, 15), text,fill=255)
     img.save(name + '.png')
