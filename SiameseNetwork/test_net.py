6e3803aedc180152d8211fc7f234310f64956b71
Jordan Ott <jordanott365@gmail.com>
2018-04-08 16:41:15 -0700

    Minor changes

--- SiameseNetwork/test_net.py
+++ SiameseNetwork/test_net.py
@@ -11,13 +11,13 @@ def plot(img1,img2,lang,pred,target,i):
     d = {1:'same',0:'different'}
     f, ax = plt.subplots(1,2)
 
-    ax[0].set_title(lang[0])
-    ax[1].set_title(lang[1])
+    ax[0].set_title(lang[0].split('/')[3])
+    ax[1].set_title(lang[1].split('/')[3])
     ax[0].set_xlabel('Predicted {}: {}'.format(d[int(np.rint(pred))],pred))
     ax[1].set_xlabel('Actual: '+str(target))
 
-    ax[0].set_axis_off()
-    ax[1].set_axis_off()
+    #ax[0].set_axis_off()
+    #ax[1].set_axis_off()
 
     ax[0].imshow(img1.reshape(200,200))
     ax[1].imshow(img2.reshape(200,200))
