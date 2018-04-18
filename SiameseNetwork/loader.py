599bc17a65c9aacfdf0c5c5e7afb842ffa15efbe
Jordan Ott <jordanott365@gmail.com>
2018-04-18 12:18:06 -0700

    Conv loader

--- SiameseNetwork/loader.py
+++ SiameseNetwork/loader.py
@@ -1,5 +1,6 @@
 import os
 import json
+import keras
 import random
 import numpy as np
 #import seaborn as sns
@@ -22,9 +23,9 @@ class Siamese_Loader:
         for i_key in data:
             self.languages[i_key] = []
             m = len(data[i_key])
-            test_index = np.random.randint(0,m,size=(lang_samples))
+            test_index = np.random.randint(0,m,size=(test_samples_per_lang))
             for t in test_index:
-                self.languages.append(DATA_DIR+data[i_key][t])
+                self.languages[i_key].append(DATA_DIR+data[i_key][t])
 
             for j_key in data:
                 # generate images to sample
@@ -86,10 +87,7 @@ class Siamese_Loader:
             pairs[1].append(img2)
             targets.append(0)
 
-        if s != 'test':
-            return [np.array(pairs[0]),np.array(pairs[1])], targets
-        else:
-            return [np.array(pairs[0]),np.array(pairs[1])], targets, np.concatenate((same_class,diff_class),axis=0)
+        return [np.array(pairs[0]),np.array(pairs[1])], targets
 
     def test_batch(self):
         lang = np.random.choice(self.languages.keys())
@@ -110,4 +108,95 @@ class Siamese_Loader:
             inputs, targets = self.test_batch()
             probs = model.predict(inputs)
             correct += 1 if np.argmax(probs) == np.argmax(targets) else 0
+        return correct/float(self.len_test)
+
+class Conv_Loader:
+    """For loading batches and testing tasks to a siamese net"""
+    def __init__(self,lang_samples,test_samples_per_lang=100):
+        self.x_train, self.y_train = [], []
+        self.x_test, self.y_test = [], []
+
+        file_path = '../DataGeneration/data.json'
+        print("loading data from {}".format(file_path))
+        with open(file_path,"rb") as f:
+            data = json.load(f)
+
+        lang_counter = 0
+        for i_key in data:
+            m = len(data[i_key])
+            test_index = np.random.randint(0,m,size=(test_samples_per_lang))
+            for t in test_index:
+                self.x_test.append(DATA_DIR+data[i_key][t])
+                self.y_test.append(lang_counter)
+
+            m = len(data[i_key])
+            train_index = np.random.randint(0,m,size=(lang_samples))
+            for t in train_index:
+                self.x_train.append(DATA_DIR+data[i_key][t])
+                self.y_train.append(lang_counter)
+
+            lang_counter += 1
+
+        self.x_train = np.array(self.x_train)
+        self.y_train = keras.utils.to_categorical(self.y_train, lang_counter+1)
+
+        self.x_test = np.array(self.x_test)
+        self.y_test = keras.utils.to_categorical(self.y_test, lang_counter+1)
+
+        self.fold_reset()
+
+    def shuffle(self,x,y):
+        c = list(zip(x, y))
+        random.shuffle(c)
+        a, b = zip(*c)
+        return np.array(a),np.array(b)
+
+    def fold_reset(self):
+        self.x_train,self.y_train = self.shuffle(self.x_train,self.y_train)
+        self.x_test,self.y_test = self.shuffle(self.x_test,self.y_test)
+
+        self.len_test = len(self.x_test)
+
+        print 'Training set'
+        print '\tLength:',self.x_train.shape
+
+        print 'Testing set'
+        print '\tLength:',self.len_test
+
+    def load_img(self,img_location):
+        img = np.array(load_img(img_location))[:,:,0].reshape(200,200,1)
+        return img/255.0
+
+    def get_batch(self,batch_size):
+        train_idx = np.random.choice(len(self.x_train),batch_size)
+        locations = self.x_train[train_idx]
+        languages = self.y_train[train_idx]
+
+        data,targets = [],[]
+        for loc,lang in zip(locations,languages):
+            img1 = self.load_img(location)
+            data.append(img1)
+            targets.append(lang)
+
+        return np.array(data), np.array(targets)
+
+    def test_batch(self,batch_size):
+        test_idx = np.random.choice(len(self.x_test),batch_size)
+        locations = self.x_test[test_idx]
+        languages = self.y_test[test_idx]
+
+        data,targets = [],[]
+        for loc,lang in zip(locations,languages):
+            img1 = self.load_img(location)
+            data.append(img1)
+            targets.append(lang)
+
+        return np.array(data), np.array(targets)
+
+    def test_oneshot(self,model,batch_size,verbose=0):
+        correct = 0
+        for b in range(0,self.len_test):
+            inputs, targets = self.test_batch()
+            probs = model.predict(inputs)
+            correct += np.sum( np.argmax(targets,axis=1) == np.argmax(probs,axis=1) ) / float(len(probs))
         return correct/float(self.len_test)
